86d35f7f1cd8ad191008d3463aded8d70a40f2a4
Timothy <78111482+tim0chiang@users.noreply.github.com>
2023-06-04 04:51:39 +0000

    New Assignment

---
+++ Assignment 1 Drawing with Text/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("#######################################################################");
+        System.out.println("##[     --- Welcome to Aardwolf MUD ---     ]##########################");
+        System.out.println("##[                                         ]############# /\"  ########");
+        System.out.println("##[      Players Currently Online: 382      ]######### _-`\"\"\"\', #######");
+        System.out.println("##[                                         ]#####  _-\"       )  ######");
+        System.out.println("##[  Game Last Rebooted on 16 July 13:47:30 ]### _-\"          |  ######");
+        System.out.println("############################################  _-\"            ;  #######");
+        System.out.println("################################## __---___-\"               |  ########");
+        System.out.println("###############################  _\"   ,,                   ;  `,,  ####");
+        System.out.println("###########################  _-\"     ;\'\'                  |  ,\'  ; ####");
+        System.out.println("#########################   _\"       \'                     `\"\'   ; ####");
+        System.out.println("##################  __---;                                    ,\' ######");
+        System.out.println("############### __\"\"  ___                                   ,\' ########");
+        System.out.println("########### _-\"\"   -\"\" _                                  ,\' ##########");
+        System.out.println("########## `-_         _                                 ; ############");
+        System.out.println("############  \"\"----\"\"\"  ;                              ; #############");
+        System.out.println("##############  /         ;                            ; ##############");
+        System.out.println("############  /            ;                          ; ###############");
+        System.out.println("##########  /               `                        ; ################");
+        System.out.println("########  /                                         ; #################");
+        System.out.println("######  /                                          ; ##################");
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println("What by thy name, adventurer?");
+        System.out.println("Computer Programming 11 - VLN");
+        System.out.println("Timothy Chiang, 03-06-2023");
+    }
+}
