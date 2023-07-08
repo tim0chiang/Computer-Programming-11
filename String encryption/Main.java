7b953f28deaa3b703a67d4c0eb93f545d5ce92be
Timothy <78111482+tim0chiang@users.noreply.github.com>
2023-07-08 05:40:13 +0000

    Almost there

--- String encryption/Main.java
+++ String encryption/Main.java
@@ -4,40 +4,34 @@ public class Main{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
-        // When someone types 'q' the game = false and the while loop will stop
-        boolean game = true;
+        // Asking the user if they want to encode or decode or simply type 'q' if they want to quit
+        System.out.println("Type 'e' if you want to encode and type 'd' if you want to decode or type 'q' if you want to quit ");
+        String user_choice = scan.nextLine();
 
-        while (game) {
-            // Getting the words that user wants to encode
-            System.out.println("Type the words that you want to encode ");
-            String user_words = scan.nextLine();
+        // Getting the words that user wants to encode
+        System.out.println("Type the words that you want to encode ");
+        String user_words = scan.nextLine();
 
-            // Make the words into an array
-            String[] arr_words = user_words.split(" ");
+        // Make the words into an array
+        String[] arr_words = user_words.split(" ");
 
-            String final_word = "";
+        String final_word = "";
 
-            // Each word starts with an "e" and the last letter is replaced with "er "
+        // Check to see if the user wants to encode or decode
+        if (user_choice.equals("e")){
+            // Each word starts with the last letter that the word originally started with and the last letter is replaced with "er "
             for (int i=0; i<arr_words.length; i++) {
-                final_word += ("e" + arr_words[i].substring(0, arr_words[i].length()-1) + "er ");
+                final_word += (arr_words[i].substring(arr_words[i].length()-1) + arr_words[i].substring(0, arr_words[i].length()-1) + "er ");
             }
-
-            // Print the words that are encoded
-            System.out.println(final_word);
-
-            // ask the user if they want the words to be decoded, try again or to quit
-            System.out.println("Type 'y' if you want to decode the words, type 'n' to try encode something again or type 'q' to quit ");
-            String user_choice = scan.nextLine();
-            // End the while loop
-            if (user_choice.equals("q")){
-                System.out.println("Thank you for playing!!!");
-                game = false;
-            }
-            // Decoding the words by just printing what the user typed out originally
-            else if (user_choice.equals("y")){
-                System.out.println("The decode words are: " + user_words);
-            }
-            System.out.println("\n ********************************************************************************** \n");
         }
+        else if (user_choice.equals("d")){
+            // Decode the words by getting rid of -er and replacing it with the letter that the word starts with
+            for (int i=0; i<arr_words.length; i++) {
+                final_word += (arr_words[i].substring(1, arr_words[i].length()-2) + arr_words[i].substring(0, 1) + " ");
+            }
+        }
+
+        // Print the words that are encoded
+        System.out.println("The word is: " + final_word);
     }
 }
